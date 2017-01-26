ad936ce56fa9fe16822e57f13ab3878208034682
travis <trvslhlt@gmail.com>
2017-01-26 19:19:24 +0000

    first commit just before starting lab_1

---
+++ lab_1/SocketProject/cmake-build-debug/CMakeFiles/SocketProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SocketProject.dir/main.c.o"
+  "CMakeFiles/SocketProject.dir/echo_client.c.o"
+  "CMakeFiles/SocketProject.dir/echo_server.c.o"
+  "SocketProject.pdb"
+  "SocketProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SocketProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
